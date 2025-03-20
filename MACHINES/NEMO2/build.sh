fc94cffe2d4b64dc1724483fc2752365eb695d8e
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2025-03-20 16:08:05 +0100

    Upated muspectre container

--- MACHINES/NEMO2/build.sh
+++ MACHINES/NEMO2/build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-apptainer build -F nemo2.sif openmpi-5.0.7_ucx-1.16.0.def 
+apptainer build -F mpibase.sif openmpi-5.0.7_ucx-1.16.0.def 
--- STACK/build.sh
+++ STACK/build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-apptainer build -F pnetcdf-1.14.0_fftw-3.3.10_pfft-master.sif pnetcdf-1.14.0_fftw-3.3.10_pfft-master.def
+apptainer build -F netcdf_fftw_pfft.sif pnetcdf-1.14.0_fftw-3.3.10_pfft-master.def
